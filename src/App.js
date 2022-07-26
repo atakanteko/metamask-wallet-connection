55fac3cf1e00ddc49a4da08c82b4751783a48bd1
atakantekoglu <atakantekoglu@gmail.com>
2022-07-26 13:59:05 +0300

    head and metamask icon

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 
 function App() {
   return (
-    <section className="h-screen flex flex-col items-center justify-center px-5">
-
-        <div className="bg-white rounded-md w-24 h-24 shadow-2xl">
-            sdf
+    <section className='h-screen flex flex-col items-center justify-center px-5'>
+        <div className='bg-white rounded-md w-full h-fit shadow-custom px-8 py-8 xl:w-4/12'>
+            <div className='flex flex-row justify-between items-center'>
+                <h2 className='text-lg'>Metamask Wallet Connect</h2>
+                <img src={require('./Assets/Images/Icons/metamask.svg').default} width={32} height={32} alt='metamask'/>
+            </div>
         </div>
     </section>
   );
